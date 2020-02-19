a18d0007a6962dcdfe99edc419cf03e2016c86f9
Szymon Kaliski <hi@szymonkaliski.com>
2020-02-19 19:19:13 +0100

    deps update

--- scripts/cache.sh
+++ scripts/cache.sh
@@ -4,4 +4,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
 NODE=/Users/szymon/.nvm/versions/node/v12*/bin/node
-$NODE ../cli.js cache -c Tracking
+$NODE ../cli.js cache
